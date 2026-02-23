e0ca41e3a0057ab4222731f06d9ef37309d6811c
CelsoMenezesdev <celso.menezesb886@gmail.com>
2026-02-23 11:47:02 -0300

    Atualizando o desafio super-trunfo nivel 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -14,7 +14,8 @@ int main() {
     float Area_1;
     float PIB_1;
     int Numero_de_Pontos_Turisticos_1;
-     
+     float densidade_Populacional_1; // Variável para calcular a densidade populacional
+    float media_PIB_PERCAPITA_1; // Variável para calcular a média do PIB da primeira carta
      // ===== CARTA 2 =====
     char Estado_2;
     char Codigo_da_Carta_2 [4];
@@ -23,6 +24,8 @@ int main() {
     float Area_2;
     float PIB_2;
     int Numero_de_Pontos_Turisticos_2;
+  float media_PIB_PERCAPITA_2; // Variável para calcular a média do PIB da segunda carta
+    float densidade_Populacional_2; // Variável para calcular a densidade populacional da segunda carta
   // Área para entrada de dados
 // ===== ENTRADA CARTA 1 =====
   printf("Digite o Estado da primeira carta (A ate H): ");
@@ -46,6 +49,8 @@ int main() {
   printf ("Digite o Número de Pontos Turísticos_1: ");
   scanf (" %i", &Numero_de_Pontos_Turisticos_1);
 
+  media_PIB_PERCAPITA_1 = (float)PIB_1 / População_1; // Cálculo da média do PIB per capita da primeira carta
+   densidade_Populacional_1 = (float)População_1 / Area_1; // Cálculo da densidade populacional da primeira carta
  // ===== ENTRADA CARTA 2 =====
   printf("\nDigite o Estado da segunda carta (A ate H): ");
   scanf(" %c", &Estado_2);
@@ -68,6 +73,8 @@ int main() {
   printf ("Digite o Número de Pontos Turísticos_2: ");
   scanf (" %i", &Numero_de_Pontos_Turisticos_2);
 
+   media_PIB_PERCAPITA_2 = (float)PIB_2 / População_2; // Cálculo da média do PIB per capita da segunda carta
+   densidade_Populacional_2 = (float)População_2 / Area_2; // Cálculo da densidade populacional da segunda carta
   // Área para exibição dos dados da cidade
     // ===== SAIDA =====
   printf ("\nPrimeira Carta: \n");
@@ -85,6 +92,10 @@ int main() {
   printf ("PIB: %.2f\n", PIB_1);
  
   printf ("Número de Pontos Turísticos: %i\n", Numero_de_Pontos_Turisticos_1);
+  
+  printf ("Densidade Populacional: %.2f habitantes/km²\n", densidade_Populacional_1); // Exibição da densidade populacional da primeira carta
+    
+  printf ("Média do PIB per capita: %.2f\n", media_PIB_PERCAPITA_1); // Exibição da média do PIB per capita da primeira carta
 
  // ===== SAIDA =====
  
@@ -103,5 +114,9 @@ int main() {
   printf ("PIB: %.2f\n", PIB_2);
   
   printf ("Número de Pontos Turísticos: %i\n", Numero_de_Pontos_Turisticos_2);
+  
+  printf ("Densidade Populacional: %.2f habitantes/km²\n", densidade_Populacional_2); // Exibição da densidade populacional da segunda carta
+
+  printf ("Média do PIB per capita: %.2f\n", media_PIB_PERCAPITA_2); // Exibição da média do PIB per capita da segunda carta
   return 0;
 } 
